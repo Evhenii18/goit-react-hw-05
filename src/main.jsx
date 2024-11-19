68e6412e00fad5cf4325f34ac2c2a0e15aa24fd6
Yevhenii Baranik <baranik18@gmail.com>
2024-11-19 12:32:42 +0000

    int

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom"; 
 import App from "./App";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+
 root.render(
 	<BrowserRouter>
-		{" "}
-		{/* Only one Router component */}
 		<App />
 	</BrowserRouter>
 );
